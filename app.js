a1360267b21b0adfc7ff58e0c2f93baec652f193
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-07 22:20:41 +0100

    add new pink box and have flashing lights box working

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const container = document.querySelector("#container");
 const box = document.createElement("div");
-// box.style.border = "1px solid black";
-// box.style.backgroundColor = "pink";
+box.style.border = "1px solid black";
+box.style.backgroundColor = "pink";
+box.style.height = "900px";
+box.style.width = "900px";
+box.style.position = "absolute";
+box.style.top = "200px";
+box.style.right = "650px";
 
-// container.appendChild(box);
+container.appendChild(box);
 
 // creates random colors for the background
 const newColor = () => {
-  //   const newNum = () => Math.floor(Math.random() * 255);
-  //   return `rgb(${newNum()},${newNum()},${newNum()})`;
-  return `rgb(255, 69, 0)`;
+  const newNum = () => Math.floor(Math.random() * 255);
+  return `rgb(${newNum()},${newNum()},${newNum()})`;
 };
 
 const body = document.querySelector("body");
-body.style.backgroundColor = newColor();
+body.style.backgroundColor = "rgb(255, 69, 0)";
 
-// const switcher = () => {
-//   return setInterval(() => {
-//     const body = document.querySelector("body");
-//     body.style.backgroundColor = newColor();
-//   }, 10000);
-// };
+const switcher = () => {
+  return setInterval(() => {
+    const body = document.querySelector("body");
+    body.style.backgroundColor = newColor();
+  }, 1000);
+};
 
 // switcher();
 
